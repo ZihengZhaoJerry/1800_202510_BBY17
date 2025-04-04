6322a28dd0a8f7f4f1365d446bda0309429548c8
Jerry Zhao <zhaojerry4@gmail.com>
2025-04-04 10:54:20 -0700

    cleared unused file

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,3 @@
-//---------------------------------
-// Your own functions here
-//---------------------------------
-
-
-//------------------------------------------------
-// Call this function when the "logout" button is clicked
-//-------------------------------------------------
 function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
